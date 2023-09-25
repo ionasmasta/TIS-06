ab08f34dac412cefb6c41677688a5abcff7fa100
MixAlena <MixAlena1@yandex.ru>
2023-09-25 14:41:29 +0300

    Create test_4.py

---
+++ pythonProject/test_4.py
@@ -0,0 +1,29 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+login_standard_user = "standard_user"
+password_all = 'secret_sauce'
+user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]')
+user_name.send_keys(login_standard_user)
+print("Input Login")
+password = driver.find_element(By.XPATH, '//*[@id="password"]')
+password.send_keys(password_all)
+print("Input Password")
+button_login = driver.find_element(By.XPATH, '//*[@id="login-button"]')
+button_login.click()
+print("Click Login Button")
+
+# driver.execute_script("window.scrollTo(0, 500)")
+time.sleep(10)
+action = ActionChains(driver)
+red_t_shirt = driver.find_element(By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[3]/a')
+action.move_to_element(red_t_shirt).perform()
+
+driver.close()
