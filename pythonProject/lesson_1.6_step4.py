d1750f882a9c3a61fe175ff60dfc1942e638f018
MixAlena <MixAlena1@yandex.ru>
2023-09-25 14:42:16 +0300

    Create lesson_1.6_step4.py

---
+++ pythonProject/lesson_1.6_step4.py
@@ -0,0 +1,29 @@
+#Импорт библиотек***
+from selenium import webdriver # Импортируем библиотеку веб-драйвера
+from selenium.webdriver.common.by import By # Импортируем библиотеку By
+import time #Импортируем библиотеку time
+
+link = "http://suninjuly.github.io/simple_form_find_task.html" # Создаем переменную по ссылке
+
+try:
+    browser = webdriver.Chrome() # Открываем Chrome
+    browser.get(link) # Переходим по ссылке указанной в переменной link
+
+    input1 = browser.find_element(By.TAG_NAME, 'input') #Находим поле для ввода firstname
+    input1.send_keys("Ivan") #вводим Ivan
+    input2 = browser.find_element(By.NAME, 'last_name') #Находим поле для ввода lastname
+    input2.send_keys("Petrov") #вводим Petrov
+    input3 = browser.find_element(By.CLASS_NAME, 'city') #Находим поле для ввода города
+    input3.send_keys("Smolensk") #вводим Smolensk
+    input4 = browser.find_element(By.ID, "country") #Находим поле для ввода страны
+    input4.send_keys("Russia") #вводим Россия
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn") #Находим кнопку
+    button.click() # нажимаем кнопку
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10) #пауза 10 секунд
+    # закрываем браузер после всех манипуляций
+    browser.quit() #закрываем окно браузера
+
+# не забываем оставить пустую строку в конце файла
