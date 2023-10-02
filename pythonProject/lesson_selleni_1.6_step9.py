852f396b37e310af2231bf3f28093ea7d5ee9139
MixAlena <MixAlena1@yandex.ru>
2023-10-02 14:03:25 +0300

    Create lesson_selleni_1.6_step9.py

---
+++ pythonProject/lesson_selleni_1.6_step9.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    first_name = browser.find_element(By.XPATH, '/html/body/div/form/div[1]/div[1]/input')
+    first_name.send_keys('John')
+
+    # last_name = browser.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input')
+    # last_name.send_keys('Doe')
+
+    email = browser.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input')
+    email.send_keys('johndoe123@gmail.com')
+
+    # email2 = browser.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input')
+    # email2.send_keys('johndoe123@gmail.com')
+
+        # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
